eb5527e85150f23e07d5c122e8f5865e3e8790f4
Aman Dangol <amandangol423@gmail.com>
2025-01-02 15:55:02 +0545

    restyling

---
+++ src/components/app-logo/app-logo.tsx
@@ -0,0 +1,21 @@
+export const AppLogo = () => {
+  return (
+    <svg width="70" viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="2"
+        y="2"
+        width="96"
+        height="46"
+        rx="23"
+        ry="23"
+        fill="#0F1123"
+        stroke="#0F1123"
+        stroke-width="4"
+      />
+
+      <circle cx="27" cy="25" r="21" fill="#fff" />
+
+      <circle cx="73" cy="25" r="21" fill="#fff" />
+    </svg>
+  );
+};
